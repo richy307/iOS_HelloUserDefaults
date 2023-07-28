d434a13661c24e7dba77b15dfba342e819cce6c1
Richy <richy@webcomm.com.tw>
2023-07-28 18:10:37 +0800

    UserDefaults.standard.set, UserDefaults.standard.value

--- iOS_HelloUserDefaults/ViewController.swift
+++ iOS_HelloUserDefaults/ViewController.swift
@@ -12,6 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Save Info to UserDefaults // set
+        UserDefaults.standard.set("Richy", forKey: "name")
+    
+        // Load Info to UserDefaults // get
+        print(UserDefaults.standard.value(forKey: "name")) // Optional(Richy)
+        
+        // optional binding
+        if let loadedName = UserDefaults.standard.value(forKey: "name") as? String {
+            print(loadedName) // Richy
+        }
+        
+        
+        let fruitArray = ["apple","banana","mango"]
+        UserDefaults.standard.set(fruitArray, forKey: "sweetFruit")
+        // UserDefaults.standard.array(forKey: "sweetFruit")
+        // UserDefaults.standard.stringArray(forKey: "sweetFruit")
+        if let loadedFruit = UserDefaults.standard.value(forKey: "sweetFruit") as? [String] {
+            print(loadedFruit[0])
+        }
+        
     }
 
 
